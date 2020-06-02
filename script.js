58f026e28b742e606c34f94bcb7173ff34d49b72
odczik <58082541+odczik@users.noreply.github.com>
2020-06-02 17:54:28 +0200

    Update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:3000")
+const socket = io("https://alterchatserver.herokuapp.com/socket.io/socket.io.js")
 const messageContainer = document.getElementById("message-container")
 const messageForm = document.getElementById("send-container")
 const messageInput = document.getElementById("message-input")
